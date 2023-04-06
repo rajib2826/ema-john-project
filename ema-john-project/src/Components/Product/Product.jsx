1688fdcfcb7772ab327067ff0bddcb17ee85da8b
rajib2826 <rajib15-2826@diu.edu.bd>
2023-04-06 12:19:11 +0600

    upload image and different tag

---
+++ ema-john-project/src/Components/Product/Product.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+  console.log(props.Product)
+  const { img, name, seller, ratings, price } = props.product;
+
+  return (
+    <div className='product'>
+      <img src={img} alt="" />
+      <div className='product-info'>
+        <h6 className='product-name'>{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturer:{seller}</p>
+        <p>Rating:{ratings}</p>
+      </div>
+      <button className='btn-cart'>Add to Cart</button>
+    </div>
+  );
+};
+
+export default Product;
