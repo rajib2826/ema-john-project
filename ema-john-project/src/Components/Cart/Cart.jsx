ea3619658ba00fd798245c6150777083bb7bd6e7
rajib2826 <rajib15-2826@diu.edu.bd>
2023-04-11 23:19:12 +0600

    add the local storage and show the UI

--- ema-john-project/src/Components/Cart/Cart.jsx
+++ ema-john-project/src/Components/Cart/Cart.jsx
@@ -9,9 +9,14 @@ const Cart = ({ cart }) => {
 
   let total = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    total = total + product.price;
+    if (product.quantity === 0) {
+      product.quantity = 1;
+    }
+    total = total + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
 
   }
 
@@ -21,7 +26,7 @@ const Cart = ({ cart }) => {
   return (
     <div className='cart'>
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price:${total} </p>
       <p>Total Shipping:${totalShipping} </p>
       <p>Tax:${tax.toFixed(2)} </p>
